6d502997822ca2406c215933e093b5adc6401af7
gabumoh <31133984+gabumoh@users.noreply.github.com>
2018-01-06 20:24:22 +0100

    Solution - Section 3 Exercise - Display customer's birthdate on their profile page.

--- VidYou/Models/MembershipType.cs
+++ VidYou/Models/MembershipType.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace VidYou.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; }
+        [Required]
         public short SignUpFee { get; set; }
         public byte DurationInMonth { get; set; }
         public byte DiscountRate { get; set; }
