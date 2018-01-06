7816fc3e54874ca92df62e03af4ab771e587df43
gabumoh <31133984+gabumoh@users.noreply.github.com>
2018-01-06 21:30:15 +0100

    Solution - Section 3 Exercise - Display the list of movies and their details using data in the database.

--- VidYou/Controllers/MoviesController.cs
+++ VidYou/Controllers/MoviesController.cs
@@ -5,64 +5,42 @@ using System.Web;
 using System.Web.Mvc;
 using VidYou.Models;
 using VidYou.ViewModels;
+using System.Data.Entity;
+
 
 namespace VidYou.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(c => c.Genre).ToList();
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movies = GetMovies().SingleOrDefault(c => c.Id == id);
+            var movies = _context.Movies.Include(c => c.Genre).SingleOrDefault(m => m.Id == id);
+
             if (movies == null)
                 return HttpNotFound();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek"},
-                new Movie { Id = 2, Name = "Wall-e"}
-            };
-        }
 
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-            return View(viewModel);
-            //return Content("Hello World!");
-            //return HttpNotFound();
-            //return new EmptyResult();
-            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name"});
-        }
-
-
-
-        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content(year + "/" + month);
-        }
     }
 
 }
--- VidYou/Models/Movie.cs
+++ VidYou/Models/Movie.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace VidYou.Models
 {
     public class Movie
     {
         public int Id { get; set; }
-        public string Name { get; set; }    
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }   
+        [Required]
+        public Genre Genre { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public byte NumberInStock { get; set; }
     }
 }
